e5e2c680f62acd030c0305c4d44130cb24e5ffef
allambin <annelise.lambin@gmail.com>
2017-06-21 20:18:19 +0200

    remove a too early table creation

--- database/migrations/2017_06_21_163141_create_libraries_table.php
+++ database/migrations/2017_06_21_163141_create_libraries_table.php
@@ -20,14 +20,6 @@ class CreateLibrariesTable extends Migration
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
         });
-        
-        Schema::create('library_books', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('library_id')->unsigned();
-            $table->integer('book_id')->unsigned();
-            $table->foreign('library_id')->references('id')->on('libraries');
-            $table->foreign('book_id')->references('id')->on('books');
-        });
     }
 
     /**
@@ -37,13 +29,6 @@ class CreateLibrariesTable extends Migration
      */
     public function down()
     {
-        
-        Schema::table('library_books', function (Blueprint $table) {
-            $table->dropForeign(['library_id']);
-            $table->dropForeign(['book_id']);
-        });
-        Schema::dropIfExists('library_books');
-        
         Schema::table('libraries', function (Blueprint $table) {
             $table->dropForeign(['user_id']);
         });
